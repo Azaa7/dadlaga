97e8fc881c01d1a06a26fbcb3c091c1c433bceec
Azaa7 <150637109+Azaa7@users.noreply.github.com>
2025-01-22 13:36:11 +0800

    changed

--- my-project/config/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBPzpVp-ytWFceE_wxJvpqFYKyk-yzcB1M",
-  authDomain: "recipe-41f8f.firebaseapp.com",
-  projectId: "recipe-41f8f",
-  storageBucket: "recipe-41f8f.firebasestorage.app",
-  messagingSenderId: "92614499864",
-  appId: "1:92614499864:web:33a2a1a0bba841f5c32d6d"
-} 
--- my-project/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const auth = useAuth()
-  
-  if (!auth.isAuthenticated && to.path !== '/login') {
-    return navigateTo('/login')
-  }
-}) 
